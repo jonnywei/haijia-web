6bc74f2bc42606e9cc8105cf5cd211052e8e6a5e
Jonny Wei <weijianjun7100@gmail.com>
2012-12-17 20:22:13 +0800

    add cookie creak model

--- settings/urls.py
+++ settings/urls.py
@@ -4,6 +4,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('settings.views',
+    url(r'^$', 'index'),
+    url(r'^imagecode/$','image_code'),
+    url(r'^bookcode/$', 'book_code'),                   
+    url(r'^imagecode/img/$','image_code_get'),
+    url(r'^bookcode/img/$', 'book_code_get'),
+    url(r'^imagecode/add/$','image_code_add'),
+    url(r'^bookcode/add/$', 'book_code_add'), 
+    
     
 )
--- settings/views.py
+++ settings/views.py
@@ -2,11 +2,13 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 import urllib2
 import cookielib
 import json
 import hashlib
+import random
 
 cj = cookielib.LWPCookieJar()
 
@@ -18,13 +20,65 @@ urllib2.install_opener(opener)
 
 
 def index(request):
-    login_pic_url ="http://haijia.bjxueche.net/tools/CreateCode.ashx?key=ImgCode&random=0.3782570156516717"
-    book_pic_url ="http://haijia.bjxueche.net/tools/CreateCode.ashx?key=BookingCode&random=0.3782570156516717"
+    login_pic_url ="http://haijia.bjxueche.net/tools/CreateCode.ashx?key=ImgCode&random="+str(random.random())
+    book_pic_url ="http://haijia.bjxueche.net/tools/CreateCode.ashx?key=BookingCode&random="+str(random.random())
 
-    #data = urllib2.urlopen(login_pic_url).read()
+    data = urllib2.urlopen(login_pic_url).read()
 
     data = urllib2.urlopen(book_pic_url).read()
     
     for cookie in cj:
         print cookie
-    return HttpResponse(data,"image/Gif")
+    return HttpResponse(data,"image/gif")
+
+
+def image_code(request):
+    
+    return render_to_response('image_code.html',{'image_code_type':'imagecode'})
+
+def book_code(request):
+    #imgage_code_type ={'image_code_type','bookcode'}
+    return render_to_response('image_code.html',{'image_code_type':'bookcode'})
+
+
+
+def image_code_add(request):
+    
+    return render_to_response('image_code.html',{'image_code_type':'imagecode'})
+
+def book_code_add(request):
+    #imgage_code_type ={'image_code_type','bookcode'}
+    return render_to_response('image_code.html',{'image_code_type':'bookcode'})
+
+
+
+
+#得到海驾的imagecode图片
+def image_code_get(request):
+
+    login_pic_url ="http://haijia.bjxueche.net/tools/CreateCode.ashx?key=ImgCode&random="+str(random.random())
+
+    data = urllib2.urlopen(login_pic_url).read()
+    
+    for cookie in cj:
+        if cookie.name == 'ImgCode':
+            print cookie.name, cookie.value
+            break
+    #response  =   HttpResponse(data,"image/gif")
+    #response.set_cookie('cookieImgCode',cookie.value)
+    #return response
+    return HttpResponse(data,"image/gif")
+
+
+#得到海驾的bookcode图片
+def book_code_get(requst):
+   
+    book_pic_url ="http://haijia.bjxueche.net/tools/CreateCode.ashx?key=BookingCode&random="+str(random.random())
+
+    data = urllib2.urlopen(book_pic_url).read()
+    
+    for cookie in cj:
+        print cookie
+    return HttpResponse(data,"image/gif")
+
+
